683111b9af5554288e240ebcfb43a9a582439231
B0050 <B0050@yungshingroup.com>
2016-03-02 11:56:00 +0800

    建建完成

---
+++ YSH/OA/P15_CHOP_APPLICATION/SignInit.java
@@ -0,0 +1,40 @@
+package YSH.OA.P15_CHOP_APPLICATION;
+//YSH/OA/P15_CHOP_APPLICATION/SignInit
+import jcx.db.talk;
+import jcx.jform.hproc;
+
+public class SignInit extends hproc{
+
+	@Override
+	public String action(String arg0) throws Throwable {
+		// TODO Auto-generated method stub
+		String sql,HECNAME,DEP_NAME;
+		talk t = getTalk();
+		if (getValue("CHOP_USER").trim().length() != 0){
+			sql = "select HECNAME,DEP_NAME from USER_INFO_VIEW where EMPID = '"+getValue("CHOP_USER")+"'";
+			String[][] ret = t.queryFromPool(sql);
+
+			HECNAME = ret[0][0];
+			DEP_NAME = ret[0][1];
+			
+			setValue("CHOP_USER_NAME",HECNAME);
+			
+		}
+		if (getValue("EMPID").trim().length() != 0){
+			sql = "select HECNAME,DEP_NAME from USER_INFO_VIEW where EMPID = '"+getValue("EMPID")+"'";
+			String[][] ret = t.queryFromPool(sql);
+
+			HECNAME = ret[0][0];
+			DEP_NAME = ret[0][1];
+			
+			setValue("EMPID_NAME",HECNAME);
+			setValue("DEPT_NO_NAME",DEP_NAME);
+		}
+		if (POSITION == 5){
+			setVisible("SEND", false);
+			setVisible("QUERYPAGE", false);
+		}
+		return arg0;
+	}
+
+}
